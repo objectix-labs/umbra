64d4130f82085b0a6dee941aa79c98f6c521a6fd
Christian Ruehl <ruehl@objectix.de>
2025-04-24 20:54:04 +0200

    added missing documentation

--- packages/umbra_core/lib/src/cipher_key.dart
+++ packages/umbra_core/lib/src/cipher_key.dart
@@ -1,11 +1,13 @@
 import 'package:cryptography_plus/cryptography_plus.dart';
 import 'package:equatable/equatable.dart';
 
+/// Represents a symmetric cipher key used for encryption and decryption
+/// of binary messages.
 class CipherKey extends Equatable {
-  final SecretKey _key;
+  final SecretKey key;
 
-  const CipherKey(this._key);
+  const CipherKey(this.key);
 
   @override
-  List<Object> get props => [_key];
+  List<Object> get props => [key];
 }
--- packages/umbra_core/lib/src/key_exchange_service.dart
+++ packages/umbra_core/lib/src/key_exchange_service.dart
@@ -3,11 +3,24 @@ import "cipher_key.dart";
 import "identity_key.dart";
 import "identity_key_pair.dart";
 
+/// A service for performing key exchange using a local identity key pair and
+/// a remote identity key.
+///
+/// This service uses the Diffie-Hellman algorithm over Curve25519 to derive a
+/// shared key for secure communication between tweo entities represeted by
+/// the given identity keys.
 class KeyExchangeService {
   final X25519 _x25519 = X25519();
 
   KeyExchangeService();
 
+  /// Dervices a shared key with a remote identity key using a local identity
+  /// key pair.
+  ///
+  /// The [myKeyPair] parameter is the local identity key pair, and the
+  /// [otherKey] parameter is the remote identity key.
+  ///
+  /// Returns a [CipherKey] representing the shared key.
   Future<CipherKey> exchange(
     IdentityKeyPair myKeyPair,
     IdentityKey otherKey,
