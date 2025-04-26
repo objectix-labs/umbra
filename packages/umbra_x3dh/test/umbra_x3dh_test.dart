175bce890b209eed2ccd120af6d3b91134b1e7b4
Christian Rühl <ruehl@objectix.de>
2025-04-26 15:13:37 +0200

    8 setup ci pipeline (#9)
    * setup test workflow
    
    * fixed flutter issues with github workflow
    
    * integrated melos action
    
    * more pipeline fixes
    
    * final fixes
    
    * added code analysis step to github workflow
    
    * activate workflow on any PR
    
    * more fixes

--- packages/umbra_x3dh/test/umbra_x3dh_test.dart
+++ packages/umbra_x3dh/test/umbra_x3dh_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:umbra_x3dh/umbra_x3dh.dart';
+//import 'package:umbra_x3dh/umbra_x3dh.dart';
 
 void main() {
   test('adds one to input values', () {});
